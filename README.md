a702103b10325bda8c180a5e1ccb1359b0a8e3ad
Kyle Lin <klin12@stuy.edu>
2018-02-26 20:54:46 -0500

    Whoops

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # space-mongos-on-earth
+
+---
+
+### How to run
+
+* Mongo intro
+    * `python ./mongo/mongo.py`: Regular version for runs
+    * `python ./mongo/mongo_pretty.py`: Run this if you like pretty outputs. Needs pprint module.
+* Mongo own datalist
+    * 
+
+---
+
+### Modules
+
+* mongo.py: Should run this
+    * pymongo
+
+* mongo_pretty.py: If you like pretty dictionaries
+    * pymongo
+    * pprint
+
+---
---
+++ mongo/mongo_pretty.py
@@ -0,0 +1,50 @@
+import pymongo, pprint
+
+# Server
+connection = pymongo.MongoClient("149.89.150.100")
+
+# Document
+restdb = connection.test
+
+# Collection
+restaurants = restdb.restaurants
+
+# Run the query
+def do_query(query):
+    results = restaurants.find(query)
+    for result in results:
+        pprint.pprint(result)
+    return results
+
+# Query from borough
+def query_borough(borough):
+    borough_qdoc = {'borough': borough}
+    return do_query(borough_qdoc)
+
+# Query from zipcode
+def query_zip(zipcode):
+    zip_qdoc = {'address.zipcode': zipcode}
+    return do_query(zip_qdoc)
+
+# Query from borough + grade
+def query_bgrade(borough, grade):
+    bgrade_qdoc = {"$and": [{'borough': borough}, {'grades.grade': grade}]}
+    return do_query(bgrade_qdoc)
+
+# Query from borough + max score
+def query_bltscore(borough, score):
+    bltscore_qdoc = {"$and": [{'borough': borough}, {'grades.score': {'$lt': score}}]}
+    return do_query(bltscore_qdoc)
+
+if __name__ == "__main__":
+    # Test borough query
+    #query_borough("Manhattan")
+
+    # Test zip query
+    #query_zip("11215")
+
+    # Test borough + grade query
+    #query_bgrade("Manhattan", "A")
+
+    # Test borough + max score query
+    query_bltscore("Manhattan", 2)
